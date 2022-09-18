23edc22b5d206f08f5ecdaf2fea3abe9b7e35a72
JesusCastroFernandez <castrofernandezjesus@gmail.com>
2022-09-18 11:53:45 +0200

    Listas
    Numeros

---
+++ src/listanumeros/ListaNumeros.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listanumeros;
+
+/**
+ *
+ * @author JESUS
+ * Año: 2021.
+ */
+public class ListaNumeros {
+
+    /**
+     * Ejercicio Lista de Números.
+     */
+    public static void main(String[] args) {
+        
+         short x = 6;         
+         System.out.println("Partiendo de una variable entera llamada x cuyo valor inicial es 6, haz que se muestre por pantalla la secuencia de números siguiente, aplicando una operación matemática sobre x:\n" +
+"\n" + "6, 10, -4, 12, 3, 2, 9, -6, 0\n" + "\n" + "Para hacer este ejercicio deberás realizar 9 operaciones matemáticas sobre la variable x, y deberás utilizar cada uno de los operadores vistos en este apartado al menos una vez.");
+         System.out.println("Lista de Números: "); 
+         System.out.println(""); 
+         System.out.println("");
+         System.out.println("x - 0");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("x + 4");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("x - 10");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("x * 2");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("x / 2");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("x % 4");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("x + 3");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("-x");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("x - x");    
+         System.out.println("................");
+         System.out.println("Fin del Programa");
+         System.out.println("----------------");
+    }
+}
